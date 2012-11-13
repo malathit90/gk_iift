2a3644bfa4861ed44d536953fb3dd77f72d9c2d9
Malathi T <malt@paypal.com>
2012-11-13 22:56:09 +0530

    Removed some print statements (for debugging)

--- gk_ques_answers/views.py
+++ gk_ques_answers/views.py
@@ -19,16 +19,14 @@ def retrieve_ques(request, ques_type_id, start_id, end_id):
     print "inside retrieve"
     ques_ans_list = fetch_ques_ans(ques_type_id, start_id, end_id)
     json_to_print = {}
-    ques_type = question_type.objects.get(pk = ques_type_id)
-    ques_type = ques_type.to_string()
-    print ques_type
+    ques_type_obj = question_type.objects.get(pk = ques_type_id)
+    ques_type = ques_type_obj.to_string()
     json_to_print['types'] = [ques_type]
     json_to_print[ques_type] = ques_ans_list
     return HttpResponse(simplejson.dumps(json_to_print), mimetype='application/json')
 
 def initial_fetch(request):
     ques_types = question_type.objects.distinct()
-    print ques_types
     all_questions = {}
     all_questions["types"] = []
     for each_type in ques_types:
